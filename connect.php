0c1af6e600402fa0bce81e3c8bebaa33f7771541
Zerone <rjdagamat@gmail.com>
2024-10-29 01:17:53 +0800

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;dbname=code_barcode_db','root','');
+
+//echo 'connection success';
+
+
+
+?>
---
+++ footer.php
@@ -0,0 +1,37 @@
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+    POS BARCODE INVENTORY SYSTEM V-2.0.
+  
+    </div>
+    <!-- Default to the left -->
+    <strong>By: F society</a>.</strong>
+    
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+
+<script src="../plugins/sweetalert2/sweetalert2.min.js"></script>
+
+</body>
+</html>
